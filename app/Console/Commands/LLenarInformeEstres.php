1530f723a51fb6e4495632b4d4e2964f10a424b2
John Aragon <ingjohnaragon@google.com>
2025-04-03 20:07:34 -0500

    Se adiciona comando para actualizar encuestas estres

---
+++ app/Console/Commands/LLenarInformeEstres.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\InformesEstresController;
+
+class LLenarInformeEstres extends Command
+{
+    protected $signature = 'informe:update-estres {registros*}';
+    protected $description = 'Actualiza o crea un reporte estres';
+
+    public function handle()
+    {
+        $registros = $this->argument('registros'); // Lista de registros pasados como argumento
+
+        if (empty($registros)) {
+            $this->error('Debes proporcionar al menos un registro.');
+            return;
+        }
+
+        $empleados = DB::table('empleados')
+            ->whereIn('registro', $registros)->get();
+        if ($empleados->isEmpty()) {
+            $this->info('No se encontraron empleados con los registros proporcionados.');
+            return;
+        }
+
+        // Prepara los datos de actualización
+        foreach ($empleados as $empleado) {
+            $fichadato = DB::table('fichadatos')
+            ->where('registro', $empleado->registro)->first();
+            if(empty($fichadato)){
+                continue;
+            }else{
+                
+                InformesEstresController::generarInformeEstres($empleado,$fichadato);
+                
+            }
+        }
+        $this->info('Informes estres actualizados correctamente para los registros: ' . implode(', ', $registros));
+    }
+}
